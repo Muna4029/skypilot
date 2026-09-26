ab8c7852eaa80f8b9075bb66c7ff1e921c345b88
rabeyakhatunmuna <rabeyakhatunmuna@Rabeyas-MacBook-Pro.local>
2026-09-25 21:22:57 -0400

    Muna4029__diff__id_13

--- sky/resources.py
+++ sky/resources.py
@@ -836,7 +836,7 @@ class Resources:
             image_size = self.cloud.get_image_size(image_id, region)
             if image_size >= self.disk_size:
                 with ux_utils.print_exception_no_traceback():
-                    size_compare = 'larger than' if image_size > self.disk_size \
+                    size_compare = 'larger than' if image_size > self.disk_size\
                         else 'equal to'
                     raise ValueError(
                         f'Image {image_id!r} is {image_size}GB, which is '
